462294fd7f6b3f5a09b239d780b0b286694f8d21
soGloomyCat <tonyhowk001@gmail.com>
2022-07-15 22:15:20 +0300

    15/07 Create Second Branch

--- Assets/Sources/NPC/NPCStateMachine/States/HangState.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-
-public class HangState : State
-{
-    private const string AnimationTrigger = "IsHang";
-
-    private void Start() => Animator.SetBool(AnimationTrigger, true);
-}
